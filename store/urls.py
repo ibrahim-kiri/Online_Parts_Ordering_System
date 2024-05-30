bf77ac0e6f162b4f4a1d22be922753c1412b7266
ibrahim-kiri <100684112+ibrahim-kiri@users.noreply.github.com>
2024-05-30 13:37:57 +0300

    Added functionality to the User Profile and Search

--- store/urls.py
+++ store/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('update_password/', views.update_password, name='update_password'),
     path('update_user/', views.update_user, name='update_user'),
+    path('update_info/', views.update_info, name='update_info'),
     path('part/<int:pk>', views.part, name='part'),
     path('category/<str:foo>', views.category, name='category'),
     path('category_summary', views.category_summary, name='category_summary'),
+    path('search/', views.search, name='search'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,13 +1,46 @@
 from django.shortcuts import render, redirect
-from .models import Part, Category
+from .models import Part, Category, Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 from django import forms
+from django.db.models import Q
 
 # Create your views here.
+def search(request):
+    # Determine if they filled out the form
+    if request.method == "POST":
+        searched = request.POST['searched']
+        # Query The Parts DB Model
+        searched = Part.objects.filter(Q(name__icontains=searched) | Q(description__icontains=searched))
+        # Test for null
+        if not searched:
+            messages.success(request, "Part Not Found!")
+            return render(request, "search.html", {})
+        else:
+            return render(request, "search.html", {'searched':searched})
+    else:
+        return render(request, "search.html", {})
+
+
+def update_info(request):
+    if request.user.is_authenticated:
+        current_user = Profile.objects.get(user__id=request.user.id)
+        form = UserInfoForm(request.POST or None, instance=current_user)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your Info Has Been Updated!")
+            return redirect('home')
+        return render(request, "update_info.html", {'form':form})
+    
+    else:
+        messages.success(request, "You Must Be Logged In To Access This Page!")
+        return redirect('home')
+
+
 def update_password(request):
     if request.user.is_authenticated:
         current_user = request.user
@@ -117,8 +150,8 @@ def register_user(request):
             # log in user
             user = authenticate(username=username, password=password)
             login(request, user)
-            messages.success(request, ("You Have Registered Successfully. Welcome!"))
-            return redirect('home')
+            messages.success(request, ("You Have Registered Successfully. Please Fill Out Your Profile Info!"))
+            return redirect('update_info')
         else:
             messages.success(request, ("There was a problem Registering, Please try again..."))
             return redirect('register')   
